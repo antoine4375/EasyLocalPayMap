fea87226c2afbe294012c51b524afec3b036683b
Taehwan Kim <antoine4375@gmail.com>
2020-08-11 10:56:23 +0900

    3. retrofit

--- app/src/main/java/com/antoine/easylocalpaymap/MainActivity.kt
+++ app/src/main/java/com/antoine/easylocalpaymap/MainActivity.kt
@@ -2,10 +2,39 @@ package com.antoine.easylocalpaymap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.antoine.easylocalpaymap.model.AuthKey
+import com.antoine.easylocalpaymap.model.LocalPayPlace
+import com.antoine.easylocalpaymap.model.service.OpenDataService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retrofit: Retrofit
+    private lateinit var service: OpenDataService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://openapi.gg.go.kr/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(OpenDataService::class.java)
+
+        service.getPlaceList(AuthKey.KEY, "json","1","1","용인시","기흥구 죽전로","").enqueue(object : Callback<LocalPayPlace>{
+            override fun onFailure(call: Call<LocalPayPlace>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call<LocalPayPlace>, response: Response<LocalPayPlace>) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
--- app/src/main/java/com/antoine/easylocalpaymap/model/service/OpenDataService.kt
+++ app/src/main/java/com/antoine/easylocalpaymap/model/service/OpenDataService.kt
@@ -1,4 +1,21 @@
 package com.antoine.easylocalpaymap.model.service
 
+import com.antoine.easylocalpaymap.model.LocalPayPlace
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface OpenDataService {
+    @GET("RegionMnyFacltStus")
+    @Headers("Content-type: application/json")
+    fun getPlaceList(
+        @Query("KEY") key: String,  // 인증키
+        @Query("Type") type: String,    // 호출 문서
+        @Query("pIndex") index: String, // 페이지 위치
+        @Query("pSize") size: String,   // 페이지당 요청 숫자
+        @Query("SIGUN_NM") sigun: String,    // 시군명
+        @Query("REFINE_ROADNM_ADDR") newAddr: String,   // 소재지도로명주소
+        @Query("REFINE_LOTNO_ADDR") oldAddr: String     // 소재지지번주소
+    ) : Call<LocalPayPlace>
 }
